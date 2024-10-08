a47dbc43d14a9fd434f4fb912980d674235a7048
İsmail Baran <ismailbaran04@gmail.com>
2024-10-08 21:57:34 +0300

    rezervasyon tablosu eklendi

---
+++ Project2WooxTravel/Models/Entities/Reservation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project2WooxTravel.Models.Entities
+{
+    public class Reservation
+    {
+        public int ReservationID { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public int PersonCount { get; set; }
+        public DateTime ReservationDate{ get; set; }
+        public string Description{ get; set; }
+    }
+}
