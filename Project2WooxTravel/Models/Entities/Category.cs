fc32a5d21307b74b24bdd90617484abc49743ff6
İsmail Baran <ismailbaran04@gmail.com>
2024-10-08 20:28:31 +0300

    proje oluşturuldu ve veri tabanı sınıfları oluşturuldu

---
+++ Project2WooxTravel/Models/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Web;
+
+namespace Project2WooxTravel.Models.Entities
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        [StringLength(100)]
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+    }
+}
