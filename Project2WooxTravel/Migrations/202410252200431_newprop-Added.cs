f78278f60bc124625563e5ebbf7639bce77a1d7a
İsmail Baran <ismailbaran04@gmail.com>
2024-10-26 03:16:49 +0300

    admin tablosuna image eklendi

---
+++ Project2WooxTravel/Migrations/202410252200431_newprop-Added.cs
@@ -0,0 +1,18 @@
+﻿namespace Project2WooxTravel.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class newpropAdded : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Admins", "ImageUrl", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Admins", "ImageUrl");
+        }
+    }
+}
